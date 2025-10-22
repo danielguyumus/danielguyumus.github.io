31eee446642d3849e03cdc677dd14cd2264b8fc3
danielguyumus <89560921+danielguyumus@users.noreply.github.com>
2025-10-22 18:26:40 +0000

    Deploying to gh-pages from @ danielguyumus/danielguyumus.github.io@702260f1c94bbd00f524596647d48ff5d94d78ba 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,9 +30,9 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/cv/";
           },
-        },{id: "nav-research",
-          title: "research",
-          description: "Research Statement.",
+        },{id: "nav-research-statement",
+          title: "Research Statement.",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/research/";
